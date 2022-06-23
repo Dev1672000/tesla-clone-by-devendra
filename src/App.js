2eb4645f2782c879206e6a06548800f55fdce2b4
Devendra Kumar Pandey <dev1672000@gmail.com>
2022-06-23 22:26:31 +0530

    aa

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
 //import { Counter } from './features/counter/Counter';
-import './App.css';
-import Header from "./components/Header"
-import Home from "./components/Home"
+import "./App.css";
+import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        Header / >
-        <
-        Home / >
-        <
-        /div>
-    );
+  return (
+    <div className="App">
+      <Header />
+
+      <Home />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,68 +1,66 @@
-import React from 'react'
-import styled from "styled-components"
-import Section from "./Section"
+import React from "react";
+import styled from "styled-components";
+import Section from "./Section";
 
 function Home() {
-    return ( <
-        Container >
-        <
-        Section title = "Model S"
-        description = "Order Online for Touchless Delivery"
-        backgroundImg = "model-s.jpg"
-        leftBtnText = "Custom order"
-        rightBtnText = "Existing inventory" /
-        >
-        <
-        Section title = "Model Y"
-        description = "Order Online for Touchless Delivery"
-        backgroundImg = "model-y.jpg"
-        leftBtnText = "Custom order"
-        rightBtnText = "Existing inventory" /
-        >
-        <
-        Section title = "Model 3"
-        description = "Order Online for Touchless Delivery"
-        backgroundImg = "model-3.jpg"
-        leftBtnText = "Custom order"
-        rightBtnText = "Existing inventory" /
-        >
-        <
-        Section title = "Model X"
-        description = "Order Online for Touchless Delivery"
-        backgroundImg = "model-x.jpg"
-        leftBtnText = "Custom order"
-        rightBtnText = "Existing inventory" /
-        >
-        <
-        Section title = "Lowest Cost Solor Panels in America"
-        description = "Money-back guarantee"
-        backgroundImg = "solar-panel.jpg"
-        leftBtnText = " order now"
-        rightBtnText = "Learn more" /
-        >
+  return (
+    <Container>
+      <Section
+        title="Model S"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-s.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model Y"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-y.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model 3"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-3.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Model X"
+        description="Order Online for Touchless Delivery"
+        backgroundImg="model-x.jpg"
+        leftBtnText="Custom order"
+        rightBtnText="Existing inventory"
+      />
+      <Section
+        title="Lowest Cost Solor Panels in America"
+        description="Money-back guarantee"
+        backgroundImg="solar-panel.jpg"
+        leftBtnText=" order now"
+        rightBtnText="Learn more"
+      />
 
-        <
-        Section title = " Solor Panels for New Roofs"
-        description = "solar Roof Costs Less Than a NEw Roof Plus Solor Panels"
-        backgroundImg = "solar-roof.jpg"
-        leftBtnText = " order now"
-        rightBtnText = "Learn more" /
-        >
+      <Section
+        title=" Solor Panels for New Roofs"
+        description="solar Roof Costs Less Than a NEw Roof Plus Solor Panels"
+        backgroundImg="solar-roof.jpg"
+        leftBtnText=" order now"
+        rightBtnText="Learn more"
+      />
 
-        <
-        Section title = "Accessories"
-        description = ""
-        backgroundImg = "accessories.jpg"
-        leftBtnText = "Shop now" /
-        >
-        <
-        /Container>
-    )
+      <Section
+        title="Accessories"
+        description=""
+        backgroundImg="accessories.jpg"
+        leftBtnText="Shop now"
+      />
+    </Container>
+  );
 }
 
-
-export default Home
-const Container = styled.div `
+export default Home;
+const Container = styled.div`
 height=100vh;
 z-index:10;
-`
+`;
